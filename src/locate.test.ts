3182160f5bffd5b0474b565f523d4b340f33517c
Joaquín Sánchez Jiménez <joaquin.sanchez@fi2net.es>
2021-09-23 13:00:01 +0200

    test: use `normalize` from 'pathe` on `locate` test

--- src/locate.test.ts
+++ src/locate.test.ts
@@ -1,10 +1,10 @@
 import { PathLike } from 'fs'
-import { resolve } from 'pathe'
+import { resolve, normalize } from 'pathe'
 
 export const locateTest = (locate: (name: string) => PathLike) => {
   test('mdi resolves the json collection', () => {
     const received = locate('mdi') as string
-    const expected = resolve('./json/mdi.json').replace(/\\/g, '/')
+    const expected = normalize(resolve('./json/mdi.json'))
     expect(received).toBe(expected)
   })
 }
